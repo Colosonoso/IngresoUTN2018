bc38f91c15c0b7b13ad5182c168b6f225aeeb9cf
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2018-02-08 12:29:22 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+
+edad=document.getElementById("edad").value;
+
+if(edad>=18)
+{
+
+alert(" es mayor de edad");
+
+}
+else(edad=18)
+{
+alert(" es menor de edad");
+
+}
 
 
 }//FIN DE LA FUNCIÓN
