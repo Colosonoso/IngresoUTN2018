2c6ec2517881bc1707c3695558d0ae41428e367d
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2018-02-14 12:30:45 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,51 @@ function Mostrar()
 //tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+
+switch(mesDelAño)
+
+{
+    case "Enero": alert("Ya pasamos el frio, ahora calor!!!");
+    break;
 
 
+    case "Febrero": alert(" Ya pasamos el invierno, ahora calor!!!");
+    break;
+
+    case "Marzo": alert("Falta para el invierno");
+    break;
+
+    
+    case "Abril": alert(" Falta para el invierno");
+    break;
 
 
+    case "Mayo": alert(" Falta para el invierno");
+    break;
+
+
+    case "Junio": alert(" Abrigate que hace frío");
+    break;
+
+    case "Julio": alert(" Abrigate que hace frío");
+    break;
+    
+    case "Agosto": alert(" Abrigate que hace frío");
+    break;
+
+
+    case "Septiembre": alert("Ya pasamos el frio, ahora calor!!! ");
+    break;
+
+    case "Octubre": alert(" Ya pasamos el frio, ahora calor!!!")
+    break;
+
+    case "Noviembre": alert("Ya pasamos el frio, ahora calor!!!");
+    break;
+   
+    
+    case "Diciembre": alert(" Felices fiestas!!!");
+    break;
+
+}
 }//FIN DE LA FUNCIÓN
