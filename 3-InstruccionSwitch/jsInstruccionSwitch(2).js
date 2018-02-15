9dcb8b420daf30c2503704a296c8e5e129b931ff
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2018-02-15 10:41:35 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -7,47 +7,26 @@ var mesDelAño = document.getElementById('mes').value;
 switch(mesDelAño)
 
 {
-    case "Enero": alert("Ya pasamos el frio, ahora calor!!!");
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+    case "Enero":
+    case "Febrero":
+    alert("Ya pasamos el frio, ahora calor!!!");
     break;
 
-
-    case "Febrero": alert(" Ya pasamos el invierno, ahora calor!!!");
-    break;
-
-    case "Marzo": alert("Falta para el invierno");
-    break;
-
-    
-    case "Abril": alert(" Falta para el invierno");
-    break;
-
-
-    case "Mayo": alert(" Falta para el invierno");
-    break;
-
-
-    case "Junio": alert(" Abrigate que hace frío");
-    break;
-
-    case "Julio": alert(" Abrigate que hace frío");
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    alert("Falta para el invierno");
     break;
     
-    case "Agosto": alert(" Abrigate que hace frío");
+    default:
+    alert("Abrigate que hace frio");
     break;
 
-
-    case "Septiembre": alert("Ya pasamos el frio, ahora calor!!! ");
-    break;
-
-    case "Octubre": alert(" Ya pasamos el frio, ahora calor!!!")
-    break;
-
-    case "Noviembre": alert("Ya pasamos el frio, ahora calor!!!");
-    break;
-   
-    
-    case "Diciembre": alert(" Felices fiestas!!!");
-    break;
 
 }
 }//FIN DE LA FUNCIÓN
