3d5e33c2a6225c05eb31db75470a099e780d0d04
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2018-02-22 10:40:43 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,71 @@
 function Mostrar()
 {
+    var SumaPos=0;
+    var SumaNeg=0;
+	var ContadorPos=0;
+	var ContadorNeg=0;
+	var CantidadPos=0;
+	var CantidadNeg=0;
+	var ContadorCeros=0;
+	var ContadorPares=0;
+	var PromedioPos;
+	var PromedioNeg;
+	var diferencia;
+	var respuesta;
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
-
-	while(respuesta!="no")
+	do	
 	{
-		
-	
+	numero=parseInt(prompt("Ingrese un numero"))
+
+
+	if(numero <0)
+{
+
+SumaNeg=SumaNeg + numero
+ContadorNeg --;
+CantidadNeg +=numero;
+
+}
+
+
+	  else if(numero>=0)
+	{
+
+SumaPos= SumaPos + numero 
+ContadorPos ++;
+CantidadPos +=numero;
+
+
+
 	}
 
+     else
+	 {
+	ContadorCeros ++;
+	 }
 
+if(numero % 2==0)
+{
+	ContadorPares ++;
+}
+	respuesta=prompt(" Desea continuar?:");	
+
+	}
+	while(respuesta=="si")
+
+	PromedioNeg= SumaNeg / ContadorNeg;
+	PromedioPos= SumaPos / ContadorPos;
+	diferencia= SumaPos - SumaNeg;
+
+document.write("1- Suma de los negativos:" + SumaNeg + "<br/>");
+document.write("2- Suma de los Positivos:" + SumaPos + "<br/>");
+document.write("3- Cantidad de los Positivos:" + CantidadPos + "<br/>");
+document.write("4- Cantidad de los Negativos:" + CantidadNeg + "<br/>");
+document.write("5- Cantidad de Ceros:" + ContadorCeros + "<br/>");
+document.write("2- Cantidad de numeros pares:" + ContadorPares + "<br/>");
+document.write("3- Promedio de los positivos:" + PromedioPos + "<br/>");
+document.write("4- Promedio de los Negativos:" + PromedioNeg + "<br/>");
+document.write("1- Diferencia entre positivos y negativos:" + diferencia + "<br/>");
 
 
 }//FIN DE LA FUNCIÓN
