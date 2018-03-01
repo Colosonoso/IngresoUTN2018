200c3a8bd0074ca512fc5f923ef87fce8a4d8523
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2018-03-01 12:27:08 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
@@ -1,6 +1,59 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+    var nota;
+    var sexo;
+    var promedio;
+    var varonesMas6=0;
+    var flag=0;
+    var AcumNota=0;
+    var notabaja;
+    
+
+for(var i=0; i<6; i++)
+
+{
+
+nota=parseInt(prompt(" Ingrese nota: "));
+
+
+while(nota <0 || nota >10)
+
+
+nota=parseInt(prompt( "Error. La nota debe estar entre el 0 y el 10. Reingrese"))
+
+}
+
+sexo=prompt(" Ingrese sexo");
+
+while(sexo !="f" && sexo!="m")
+
+{
+
+sexo=prompt(" Error, el sexo debe ser f o m. Reingrese");
+
+}
+
+AcumNota+=nota;
+
+
+if(nota< notabaja || flag==0)
+
+{
+    notabaja=nota;
+    flag=1;
+}
+
+if(sexo=="m" && nota>=0)
+{
+    varonesMas6++;
+}
+
+promedio=AcumNota/6;
+
+alert(" Promedio de notas" + promedio);
+alert("Nota más baja" + notabaja);
+alert("Varones con nota >=6"+ varonesMas6);
+
 }
 
--- ExamenIngresoViejo/codigoFuenteParcial/8-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/8-iteraciones.js
@@ -1,6 +1,24 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+    var numero;
+    var ContPares=0;
+    var acumulador=0;
+    var contador=0;
+    var promedio;
+    var maximo;
+    var minimo;
+    var flag=0;
+    var seguir;
+
+    do
+    {
+        numero= parseInt(prompt(" Ingrese un numero :"));
+
+        while(numero <0)
+        {
+            numero=parseInt(prompt(" Error el numero debe ser positivo. Reingrese nuevamente:"));
+        }
+    }
 }
 
